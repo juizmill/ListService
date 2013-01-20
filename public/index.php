a0a7f9d945654662b1b2a25833f9ceace681e6e6
Jesus Vieira <jesusvieiradelima@gmail.com>
2013-01-20 20:49:03 -0300

    Primeiro commit.

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+//Config for development
+ini_set('display_errors', true);
+error_reporting(E_ALL | E_STRICT);
+
+date_default_timezone_set('America/Campo_Grande');
+
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
