f22dd4d8fb5fbc75735c48faf9909ff31d973d81
Jesus Vieira <jesusvieiradelima@gmail.com>
2013-01-21 20:11:06 -0300

    Fix #62 – Fixute – PRIORITY concluído

---
+++ module/LSPriority/src/LSPriority/Fixture/LodaPriority.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Este arquivo é responsável por testar a manipulação de registro no banco de dados.
+ * Onde o mesmo se refere a tabela priority.
+ * 
+ * @author Jesus Vieira E-mail <jesusvieiradelima@gmail.com>
+ * @package LSCategoryTicket\Fixture
+ */
+
+namespace LSPriority\Fixture;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use LSPriority\Entity\Priority;
+
+class LodaPriority extends AbstractFixture
+{
+  public function load(\Doctrine\Common\Persistence\ObjectManager $manager)
+  {
+    $priority = new Priority;
+    $priority->setDescription('Alta')->setActive(true);
+    $manager->persist($priority);
+    
+    $priority1 = new Priority;
+    $priority1->setDescription('Baixa')->setActive(true);
+    $manager->persist($priority1);
+    
+    $priority2 = new Priority;
+    $priority2->setDescription('Média')->setActive(true);
+    $manager->persist($priority2);
+    
+    $manager->flush();
+  }
+}
