284d13fcb0c3665c1548710701c2d099ce9ba8b0
Jesus Vieira <jesusvieiradelima@gmail.com>
2013-01-30 20:54:34 -0300

    View - edit TYPE_USER Concluído

--- module/LSTypeuser/src/LSTypeuser/Controller/TypeUserController.php
+++ module/LSTypeuser/src/LSTypeuser/Controller/TypeUserController.php
@@ -17,13 +17,14 @@ use LSBase\Controller\CrudController;
 class TypeUserController extends CrudController
 {
 
-    public function __construct()
+    public function __construct ()
     {
         $this->controller = 'type-user';
         $this->entity = 'LSTypeuser\Entity\TypeUser';
         $this->form = 'LSTypeuser\Form\TypeUser';
         $this->service = 'LSTypeuser\Service\TypeUser';
         $this->route = 'type-user';
+
     }
 
     /**
@@ -35,35 +36,86 @@ class TypeUserController extends CrudController
      * @access public
      * @return \Zend\View\Model\ViewModel
      */
-    public function newAction()
+    public function newAction ()
     {
 
         $form = new $this->form();
 
-        $request = $this->getRequest();
+        $request = $this->getRequest ();
 
-        if ($request->isPost()) {
+        if ( $request->isPost () ) {
 
-            $form->setData($request->getPost());
+            $form->setData ($request->getPost ());
 
-            $data = $request->getPost()->toArray();
+            $data = $request->getPost ()->toArray ();
 
-            $duplicate = $this->getEm()->getRepository($this->entity)->findOneBy(array('description' => $data['description']));
+            $duplicate = $this->getEm ()->getRepository ($this->entity)->findOneBy (array('description' => $data['description']));
 
-            if ($duplicate) {
-                return new ViewModel(array('form' => $form, 'duplicate' => 'Já existe um cadastrado com este nome!'));
+            if ( $duplicate ) {
+                return new ViewModel (array('form' => $form, 'duplicate' => 'Já existe um cadastrado com este nome!'));
             }
 
-            if ($form->isValid()) {
+            if ( $form->isValid () ) {
 
-                $service = $this->getServiceLocator()->get($this->service);
-                $service->insert($data);
+                $service = $this->getServiceLocator ()->get ($this->service);
+                $service->insert ($data);
 
-                return $this->redirect()->toRoute($this->route, array('controller' => $this->controller));
+                return $this->redirect ()->toRoute ($this->route, array('controller' => $this->controller));
             }
         }
 
-        return new ViewModel(array('form' => $form));
+        return new ViewModel (array('form' => $form));
+
+    }
+
+    /**
+     * editAction
+     * 
+     * Exibe pagina para editar o registro.
+     * 
+     * @author Jesus Vieira <jesusvieiradelima@gmail.com>
+     * @access public
+     * @return \Zend\View\Model\ViewModel
+     */
+    public function editAction ()
+    {
+        $form = new $this->form();
+
+        $request = $this->getRequest ();
+        $param = $this->params ()->fromRoute ('id', 0);
+
+        $repository = $this->getEm ()->getRepository ($this->entity);
+        $entity = $repository->find ($param);
+
+        if ( $entity ) {
+
+            $form->setData ($entity->toArray ());
+
+            if ( $request->isPost () ) {
+
+                $form->setData ($request->getPost ());
+
+                $data = $request->getPost ()->toArray ();
+
+                $duplicate = $this->getEm ()->getRepository ($this->entity)->findOneBy (array('description' => $data['description']));
+
+                if ( $duplicate ) {
+                    return new ViewModel (array('form' => $form, 'id' => $param, 'duplicate' => 'Já existe um cadastrado com este nome!'));
+                }
+                if ( $form->isValid () ) {
+
+                    $service = $this->getServiceLocator ()->get ($this->service);
+                    $service->update ($data);
+
+                    return $this->redirect ()->toRoute ($this->route, array('controller' => $this->controller));
+                }
+            }
+        } else {
+            return $this->redirect ()->toRoute ($this->route, array('controller' => $this->controller));
+        }
+
+        return new ViewModel (array('form' => $form, 'id' => $param));
+
     }
 
 }
