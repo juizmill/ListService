0deda73fef17ab96ad95bc2d6dd9a020533e65f9
Jesus Vieira <jesusvieiradelima@gmail.com>
2013-01-30 20:39:35 -0300

    Em todos os módulos foram transferido a chamada do layout e do error para o módulo LSBase

--- module/LSPriority/config/module.config.php
+++ module/LSPriority/config/module.config.php
@@ -59,9 +59,9 @@ return array(
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
         'template_map' => array(
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
+            'layout/layout' => __DIR__ . '/../../LSBase/view/layout/layout.phtml',
+            'error/404' => __DIR__ . '/../../LSBase/view/error/404.phtml',
+            'error/index' => __DIR__ . '/../../LSBase/view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
